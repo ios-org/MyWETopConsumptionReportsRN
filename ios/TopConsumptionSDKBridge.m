76e18045f539fa78aee610abb7ca83f701490653
Ranahossam156 <ranahossam156@gmail.com>
2026-05-13 19:09:44 +0300

    Bridging completed

--- ios/TopConsumptionSDKBridge.m
+++ ios/TopConsumptionSDKBridge.m
@@ -6,3 +6,11 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(TopConsumptionSDKBridge, NSObject)
+
+RCT_EXTERN_METHOD(openTopReports:(NSString *)serviceNumber)
+RCT_EXTERN_METHOD(openConsumption:(NSString *)subscriberId)
+
+@end
--- ios/TopConsumptionSDKBridge.swift
+++ ios/TopConsumptionSDKBridge.swift
@@ -1,8 +1,38 @@
-//
-//  TopConsumptionSDKBridge.swift
-//  MyWETopConsumptionReportsRN
-//
-//  Created by Rana on 13/05/2026.
-//
-
 import Foundation
+import UIKit
+import React
+import TopConsumptionSDK 
+
+@objc(TopConsumptionSDKBridge)
+class TopConsumptionSDKBridge: NSObject {
+
+  @objc(openTopReports:)
+  func openTopReports(serviceNumber: String) {
+    DispatchQueue.main.async {
+      guard let rootVC = UIApplication.shared.windows.first?.rootViewController else { return }
+      
+      let sdkVC = TopConsumptionSDK.initiateTopReports(serviceNumber: serviceNumber)
+      
+      sdkVC.modalPresentationStyle = .fullScreen 
+      
+      rootVC.present(sdkVC, animated: true, completion: nil)
+    }
+  }
+
+  @objc(openConsumption:)
+  func openConsumption(subscriberId: String) {
+    DispatchQueue.main.async {
+      guard let rootVC = UIApplication.shared.windows.first?.rootViewController else { return }
+      
+      let sdkVC = TopConsumptionSDK.initiateConsumptionReports(subscriberId: subscriberId)
+      
+      sdkVC.modalPresentationStyle = .fullScreen 
+      
+      rootVC.present(sdkVC, animated: true, completion: nil)
+    }
+  }
+
+  @objc static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
+}
